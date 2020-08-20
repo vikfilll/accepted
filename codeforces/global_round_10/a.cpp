b8985850a0736beadf436ca2332ee3a280552396
HOME-PC\Viktor <vfilyov@list.ru>
2020-08-20 21:56:57 +0300

    no message

--- codeforces/global_round_10/a.cpp
+++ codeforces/global_round_10/a.cpp
@@ -16,41 +16,30 @@
 #include <stdlib.h>
 #include <numeric>
 #include <iomanip>
-#define ll long long
-#define pb push_back
-#define sz(x) (int)(x).size()
-#define fi first
-#define ins insert
-#define se second
-#define all(x) (x).begin(), (x).end()
-#define vi vector <int>
-#define vvi vector<vector<int>>
-#define k1(x) __builtin_popcount(x)
-#define sl0(x) __builtin_clz(x)
-#define sp0(x) __builtin_ctz(x)
-#define pii pair <int, int>
-#define pdd pair <double, double>
-#define vpii vector <pii>
-#define vvpii vector<vector<pii>>
-const double PI = acos(-1);
+const double PI = 3.14159265358979323846;
 const int maxn = 2e5 + 19;
 const int INF = 2e9;
-const ll MOD = 1234567891;
 using namespace std;
-int n, m, k, qq;
+int n, m, k;
+
 void solve() {
     cin >> n;
-    vi a(n);
+    vector <int> a(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
     for (int i = 0; i < n - 1; ++i) {
-        if (a[i] != a[i + 1]) { cout << 1 << '\n'; return; }
+        if (a[i] != a[i + 1]) {
+            cout << 1 << '\n';
+            return;
+        }
     }
     cout << n << '\n';
 }
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    int qq;
     cin >> qq;
     while (qq--) solve();
     return 0;
